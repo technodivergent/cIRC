fe91b9f66ce2ffbb501e7f14c18528f975dcd260
George Hall <georgemhall@users.noreply.github.com>
2017-12-08 17:28:41 -0500

    Now with more pthreads!

--- server.c
+++ server.c
@@ -6,12 +6,19 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sys/socket.h>
+#include <pthread.h>
+
+void* thread(void* argp);
+void request_handler(int conn);
 
 int verbose;
-int listenfd, conn, clientlen;
+int listenfd, *conn, clientlen;
 struct sockaddr_in client;
+char* client_addr;
+int client_port;
+pthread_t tid;
 
-void start(char* port) {
+void create_connection(char* port) {
     int b_ReuseAddr = 1; // Specify to reuse socket address
     int b_bindSuccess, b_listenSuccess;
     
@@ -20,10 +27,10 @@ void start(char* port) {
     setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &b_ReuseAddr, sizeof(b_ReuseAddr));
     if(verbose) {
         if(listenfd <= 0) {
-            perror("[P] Error creating socket...\n");
+            perror("Error creating socket...\n");
             exit(EXIT_FAILURE);
         } else {
-            printf("[P] socket created\n");
+            printf("socket created\n");
         }
     }
     
@@ -35,10 +42,10 @@ void start(char* port) {
     if(verbose) {
         if(b_bindSuccess < 0)
         {
-            perror("[P] bind failed!\n");
+            perror("bind failed!\n");
             exit(EXIT_FAILURE);
         } else {
-            printf("[P] bind success\n");
+            printf("bind success\n");
         }
     }
     
@@ -46,19 +53,37 @@ void start(char* port) {
     b_listenSuccess = listen(listenfd, 1);
     if(verbose) {
         if(b_listenSuccess < 0) {
-            perror("[P] listen failed!\n");
+            perror("listen failed!\n");
             exit(EXIT_FAILURE);
         } else {
-            printf("[P] listen success\n");
+            printf("listen success\n");
         }
+    }
+    
+    // accept incoming connections
+    while (1) {
+        printf("Waiting for incoming connection...\n");
+        
+        clientlen = sizeof(struct sockaddr_in);
+        conn = malloc(sizeof(int));
+        *conn = accept(listenfd, (struct sockaddr*) &client, (socklen_t*) &clientlen);
+        client_addr = inet_ntoa(client.sin_addr);
+        client_port = (int) ntohs(client.sin_port);
+        pthread_create(&tid, NULL, thread, conn);
+        
+        if(conn < 0 ) {
+            perror("accept failed\n");
+            exit(EXIT_FAILURE);
+        } else {
+            printf("Connected to %s:%i\n", client_addr, client_port);
+        }
+        
+        sleep(1);
     }
 }
 
 int main(int argc, char** argv) {
     char* port = argv[1];
-    char client_request[200];
-    char server_reply[100];
-    const char* hello = "hello\r\n";
     
     if(argc <= 1){
         printf("Usage: %s <port> [-v]\n", argv[0]);
@@ -70,66 +95,44 @@ int main(int argc, char** argv) {
     else
         verbose = 0;
     
-    // SETUP THE CONNECTION
-    start(port);
-    
-    // accepting incoming connections
-    while (1) {
-        printf("[P] Waiting for incoming connection...\n");
-        
-        clientlen = sizeof(struct sockaddr_in);
-        conn = accept(listenfd, (struct sockaddr*) &client, (socklen_t*) &clientlen);
-        
-        if(conn < 0 ) {
-            perror("[P] accept failed\n");
-            exit(EXIT_FAILURE);
-        } else {
-            printf("[P] Connection accepted\n");
-        }
-        
-        // Child handles requests/replies
-        if(fork() == 0) {
-            close(listenfd); // close parent listen socket
-            
-            printf("[C] Waiting for incoming request...\n");
-            
-            // Clean variables from previous requests/replies
-            memset(client_request, '\0', sizeof(client_request));
-            memset(server_reply, '\0', sizeof(server_reply));
-            
-            // Receive a request
-            if(recv(conn, client_request, 200, 0) < 0) {
-                printf("[C] recv failed!\n");
-                break;
-            } else {
-                printf("[C] Client request: %s\n", client_request);
-            }
-            
-            // Determine what to send
-            if(strcmp(hello, client_request) == 0) {
-                strcpy(server_reply, "hi!\n");
-            } else {
-                strcpy(server_reply, "what?\n");
-            }
-
-            // Send a reply
-            if(send(conn, server_reply, strlen(server_reply), 0) < 0) {
-                printf("[C] Send failed!\n");
-                exit(EXIT_FAILURE);
-            } else {
-                printf("[C] Server reply: %s\n", server_reply);
-            }
-            
-            // close child connection with client and exit successfully
-            printf("[C] Closing connection...\n");
-            close(conn);
-            exit(EXIT_SUCCESS);
-        }
-        
-        close(conn);
-        sleep(1);
-    }
+    // Create connection which will run until
+    // the last client disconnects or the server quits
+    create_connection(port);
     
     return (EXIT_SUCCESS);
 }
 
+void* thread(void *vargp) {
+    int conn = *((int*) vargp);
+    pthread_detach(pthread_self());
+    free(vargp);
+    request_handler(conn);
+    close(conn);    
+    return NULL;
+}
+
+void request_handler(int conn) {
+    //const char* hello = "hello\r\n";
+    char client_request[200];
+    char server_reply[100];
+    
+    while(recv(conn, client_request, 200, 0) != 0) {
+        // Do the things
+        printf("request from %s:%i: %s", client_addr, client_port, client_request);
+        
+        if(strcmp("HELO\r\n", client_request) == 0) {
+            strcpy(server_reply, "> hello!\r\n");
+            printf("reply to %s:%i: %s", client_addr, client_port, server_reply);
+        } else if(strcmp("QUIT\r\n", client_request) == 0) {
+            strcpy(server_reply, "> goodbye!\r\n");
+            printf("client %s:%i is disconnecting\n", client_addr, client_port);
+            return;
+        }
+        
+        send(conn, server_reply, strlen(server_reply), 0);
+        
+        // Clear variables for next iteration
+        memset(client_request, '\0', sizeof(client_request));
+        memset(server_reply, '\0', sizeof(server_reply));
+    }
+}
